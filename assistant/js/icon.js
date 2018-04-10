bc8cf042425759ccfec96236483d9641c242c9de
zhouhui <1005654889@qq.com>
2018-04-10 21:14:33 +0800

    'b5'

--- assistant/js/icon.js
+++ assistant/js/icon.js
@@ -1,1 +1,1 @@
-document.write('<script src="https://at.alicdn.com/t/font_615981_83zyfzyx0f0cc8fr.js"></script>');
+document.write('<script src="https://at.alicdn.com/t/font_615981_8b5akoifdgvf5hfr.js"></script>');
